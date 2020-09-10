aef6d2fe32156315c71f16bd09f4b6f5de536874
João Rafael <jrsc2@cin.ufpe.br>
2020-09-09 23:27:41 -0300

    Reworked propulsion: speed limit and instance variables

--- Project/Propulsion.gd
+++ Project/Propulsion.gd
@@ -5,10 +5,29 @@ export var accel = 300
 export var max_speed = 100
 export var friction = 500
 
-func get_move_towards_input(movement: Vector2, input: Vector2, delta):
-	if input == Vector2.ZERO:
-		return get_brake(movement, delta)
-	return movement.move_toward(input * max_speed, accel * delta)
+var movement: Vector2
+var input: Vector2
+var delta: float
 
-func get_brake(movement: Vector2, delta):
-	return movement.move_toward(Vector2.ZERO, friction * delta)
+func get_move_towards_input(_movement: Vector2, _input: Vector2, _delta):
+	movement = _movement
+	input = _input.normalized()
+	delta = _delta
+	
+	if input == Vector2.ZERO:
+		brake()
+	else:
+		accelerate()
+		limit_to_max_speed()
+	return movement
+
+func accelerate():
+	movement = movement.move_toward(input.normalized() * max_speed, delta * accel)
+	print(movement)
+
+func brake():
+	movement = movement.move_toward(Vector2.ZERO, friction * delta)
+	
+func limit_to_max_speed():	
+	if movement.length() > max_speed:
+		movement = movement.normalized() * max_speed
